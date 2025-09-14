c9c39fda5000b3af95933758c994e60612b357ab
TacAttack-04 <goddlycat@gmail.com>
2025-09-14 00:29:10 -0500

    Update entrypoint.sh

--- startup/entrypoint.sh
+++ startup/entrypoint.sh
@@ -2,8 +2,8 @@
 # /startup/entrypoint.sh
 
 echo "Running some quick checks ..."
-./identity-test.sh
+./startup/identity-test.sh
 
 : "${CRON_SCHEDULE:=0 */12 * * *}"  # default to every 12 hours
-echo "$CRON_SCHEDULE /aur-build-mirror.sh >> /var/log/cron.log 2>&1" > /etc/crontab
+echo "$CRON_SCHEDULE /startup/aur-build-mirror.sh >> /var/log/cron.log 2>&1" > /etc/crontab
 crond -n
