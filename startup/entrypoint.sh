8485c6bbd5ecc36019b9bd1dcb2467e3488513ed
TacAttack-04 <goddlycat@gmail.com>
2025-09-14 00:49:42 -0500

    Update entrypoint.sh
    fixing cron

--- startup/entrypoint.sh
+++ startup/entrypoint.sh
@@ -4,6 +4,8 @@
 echo "Running some quick checks ..."
 ./startup/identity-test.sh
 
+su - myuser -c 'echo "0 * * * * /home/myuser/myscript.sh" | crontab -'
+
 : "${CRON_SCHEDULE:=0 */12 * * *}"  # default to every 12 hours
-echo "$CRON_SCHEDULE /startup/aur-build-mirror.sh >> /var/log/cron.log 2>&1" > /etc/crontab
-crond -n
+su - builder -c "echo '$CRON_SCHEDULE /startup/aur-build-mirror.sh >> /var/log/cron.log 2>&1' | crontab -"
+exec crond -n
