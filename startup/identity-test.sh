af1230557b5680ec4a4d5fd5d63c31283e9e63e1
TacAttack-04 <goddlycat@gmail.com>
2025-09-14 00:33:24 -0500

    Update identity-test.sh

--- startup/identity-test.sh
+++ startup/identity-test.sh
@@ -4,7 +4,7 @@
 set -e
 echo "user: $(whoami)"
 echo "home directory: $HOME"
-ehco "working directory: $(pwd)"
+echo "working directory: $(pwd)"
 
 if [ "$EUID" -eq 0 ]; then
     echo "ERROR: Your user is set to root it must be changed"
