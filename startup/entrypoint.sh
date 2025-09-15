9aa062e1b0bd8d5bd75e8dfb35932cfbce9d83c5
TacAttack-04 <goddlycat@gmail.com>
2025-09-15 18:12:28 -0500

    Update entrypoint.sh

--- startup/entrypoint.sh
+++ startup/entrypoint.sh
@@ -4,19 +4,9 @@
 echo "Running some quick checks ..."
 ./startup/identity-test.sh
 
-: "${CRON_SCHEDULE:=0 */12 * * *}"  # default to every 12 hours
-# sudo echo "$CRON_SCHEDULE /startup/aur-build-mirror.sh >> /var/log/cron.log 2>&1" | crontab -
-# exec crond -n -p /tmp/crond.pid
-
-# Parse cron schedule to seconds (basic example for */12 hours)
-INTERVAL_HOURS=$(echo "$CRON_SCHEDULE" | grep -o '\*/[0-9]*' | cut -d'/' -f2)
-INTERVAL_SECONDS=$((${INTERVAL_HOURS:-12} * 3600))
-
-# Run once immediately (optional)
-./startup/aur-build-mirror.sh
-
-# Then run in loop
-while true; do
-    sleep $INTERVAL_SECONDS
-    ./startup/aur-build-mirror.sh
-done
+echo "${CRON_SCHEDULE:=0 */12 * * *} ./startup/aur-build-mirror.sh" > /schedule.cron
+if [ "$CRON_TESTED" != "true" ]; then
+    supercronic -test /schedule.cron
+    export CRON_TESTED=true
+fi
+supercronic -l info /schedule.cron
